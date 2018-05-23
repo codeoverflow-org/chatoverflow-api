86fb9ea0e95bce6aa7e4613a1ce8a3182e790c7d
sebinside <hi@sebinside.de>
2018-05-23 16:22:58 +0200

    Testing plugin configuration.

--- src/main/java/org/codeoverflow/chatoverflow/api/plugin/configuration/Configuration.java
+++ src/main/java/org/codeoverflow/chatoverflow/api/plugin/configuration/Configuration.java
@@ -8,8 +8,31 @@ import java.util.Map;
 
 public class Configuration {
 
-    Map<String, SourceRequirement<? extends Input>> inputs = new HashMap<>();
-    Map<String, SourceRequirement<? extends Output>> outputs = new HashMap<>();
-    Map<String, ParameterRequirement<String>> parameters = new HashMap<>();
+    private Map<String, SourceRequirement<? extends Input>> inputs = new HashMap<>();
+    private Map<String, SourceRequirement<? extends Output>> outputs = new HashMap<>();
+    private Map<String, ParameterRequirement<String>> parameters = new HashMap<>();
 
+    public void addInputRequirement(String name, SourceRequirement<? extends Input> requirement) {
+        this.inputs.put(name, requirement);
+    }
+
+    public void addOutputRequirement(String name, SourceRequirement<? extends Output> requirement) {
+        this.outputs.put(name, requirement);
+    }
+
+    public void addParameterRequirement(String name, ParameterRequirement<String> requirement) {
+        this.parameters.put(name, requirement);
+    }
+
+    public Map<String, SourceRequirement<? extends Input>> getInputs() {
+        return inputs;
+    }
+
+    public Map<String, SourceRequirement<? extends Output>> getOutputs() {
+        return outputs;
+    }
+
+    public Map<String, ParameterRequirement<String>> getParameters() {
+        return parameters;
+    }
 }
