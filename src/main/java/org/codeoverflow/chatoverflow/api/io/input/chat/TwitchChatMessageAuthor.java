3f50943ce6096965db3208490070b26e44604ef4
derNiklaas <derNiklaas@users.noreply.github.com>
2018-12-02 18:28:47 +0100

    Added Twitch VIP to Code Overflow

--- src/main/java/org/codeoverflow/chatoverflow/api/io/input/chat/TwitchChatMessageAuthor.java
+++ src/main/java/org/codeoverflow/chatoverflow/api/io/input/chat/TwitchChatMessageAuthor.java
@@ -3,18 +3,24 @@ package org.codeoverflow.chatoverflow.api.io.input.chat;
 public class TwitchChatMessageAuthor extends ChatMessageAuthor {
 
     private boolean premium;
+    private boolean vip;
 
     public TwitchChatMessageAuthor(String displayName) {
         super(displayName);
         this.premium = false;
     }
 
-    public TwitchChatMessageAuthor(String displayName, boolean broadcaster, boolean moderator, boolean subscriber, boolean premium) {
+    public TwitchChatMessageAuthor(String displayName, boolean broadcaster, boolean moderator, boolean subscriber, boolean premium, boolean vip) {
         super(displayName, broadcaster, moderator, subscriber);
         this.premium = premium;
+        this.vip = vip;
     }
 
     public boolean isPremium() {
         return premium;
+    }
+    
+    public boolean isVIP() {
+        return vip;
     }
 }
