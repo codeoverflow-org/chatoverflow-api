df425fcfdd6a7b1f64682165e9734d5f3c1d9e72
Jonas <joblo.2213@gmail.com>
2019-05-12 09:56:29 +0200

    Further improved documentation

--- src/main/java/org/codeoverflow/chatoverflow/api/io/Serializable.java
+++ src/main/java/org/codeoverflow/chatoverflow/api/io/Serializable.java
@@ -4,7 +4,18 @@ package org.codeoverflow.chatoverflow.api.io;
  * Allows serializing a object to a string, useful for saving to config
  */
 public interface Serializable {
+
+    /**
+     * Serializes this object into a string to save it to a config
+     *
+     * @return serialized
+     */
     String serialize();
 
+    /**
+     * Deserialize a string to apply provided config settings to this object
+     *
+     * @param value should be serialized
+     */
     void deserialize(String value);
 }
