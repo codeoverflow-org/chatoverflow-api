ee2aac5a5406ac205e854bf1caf3792cce34c453
Jonas <joblo.2213@gmail.com>
2019-05-15 17:55:53 +0200

    Add default implementations for input/output

--- src/main/java/org/codeoverflow/chatoverflow/api/io/input/chat/TwitchChatInput.java
+++ src/main/java/org/codeoverflow/chatoverflow/api/io/input/chat/TwitchChatInput.java
@@ -7,14 +7,14 @@ import org.codeoverflow.chatoverflow.api.io.input.chat.ChatInput;
 /**
  * Input connecting to the Twitch Internet Relay Chat, allows public and private channels
  * <p>
- * <b>Note:</b><br>
- * To be consistent with other inputs, there is currently only one input channel allowed<br>
- * For changing the twitch channel see {@link #setChannel(String)}
+ * <b>Important notice:</b><br>
+ * <b>Call {@link #setChannel(String)} to bind this input to a channel before performing any other action</b>}
  */
 public interface TwitchChatInput extends ChatInput<TwitchChatMessage>, PrivateChatInput<TwitchChatMessage> {
 
     /**
      * Changes the twitch channel to input from
+     *
      * @param channel name of the channel
      */
     void setChannel(String channel);
--- src/main/java/org/codeoverflow/chatoverflow/api/io/output/chat/TwitchChatOutput.java
+++ src/main/java/org/codeoverflow/chatoverflow/api/io/output/chat/TwitchChatOutput.java
@@ -2,7 +2,16 @@ package org.codeoverflow.chatoverflow.api.io.output.chat;
 
 /**
  * Output to send messages in the twitch chat
+ * <p>
+ * <b>Important notice:</b><br>
+ * <b>Call {@link #setChannel(String)} to bind this input to a channel before performing any other action</b>}
  */
 public interface TwitchChatOutput extends ChatOutput {
 
+    /**
+     * Changes the twitch channel to output to
+     *
+     * @param channel name of the channel
+     */
+    void setChannel(String channel);
 }
