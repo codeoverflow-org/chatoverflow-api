7c8553d60612c8a5b404de37dc1e430d54a7174b
Jonas <joblo.2213@gmail.com>
2019-05-25 08:47:18 +0200

    Rework of discord service. Added functionality: * Send & Receive embeds * Send & Receive files * Receive reactions
    Improved:
    * Less duplicated code
    * Updated JDA to latest version

--- src/main/java/org/codeoverflow/chatoverflow/api/io/dto/chat/discord/DiscordChatMessage.java
+++ src/main/java/org/codeoverflow/chatoverflow/api/io/dto/chat/discord/DiscordChatMessage.java
@@ -1,30 +1,53 @@
 package org.codeoverflow.chatoverflow.api.io.dto.chat.discord;
 
 import org.codeoverflow.chatoverflow.api.io.dto.Identifiable;
-import org.codeoverflow.chatoverflow.api.io.dto.chat.ChatEmoticon;
 import org.codeoverflow.chatoverflow.api.io.dto.chat.ChatMessage;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * A chat message that was send in a discord channel
  */
 public class DiscordChatMessage extends ChatMessage<DiscordChatMessageAuthor, DiscordChannel, DiscordChatCustomEmoticon> implements Identifiable {
 
-    private String id;
+    private final String id;
+    private final DiscordEmbed embed;
+    private final List<String> attachmentUrls;
+    private final List<DiscordReaction> reactions;
 
     public DiscordChatMessage(DiscordChatMessageAuthor author, String message, Long timestamp, DiscordChannel channel, String id) {
         super(author, message, timestamp, channel);
         this.id = id;
+        this.embed = null;
+        this.attachmentUrls = new ArrayList<>();
+        reactions = new ArrayList<>();
     }
 
-    public DiscordChatMessage(DiscordChatMessageAuthor author, String message, Long timestamp, DiscordChannel channel, List<DiscordChatCustomEmoticon> emoticons, String id) {
+    public DiscordChatMessage(DiscordChatMessageAuthor author,
+                              String message,
+                              Long timestamp,
+                              DiscordChannel channel,
+                              List<DiscordChatCustomEmoticon> emoticons,
+                              String id) {
+        this(author, message, timestamp, channel, emoticons, null, new ArrayList<>(), new ArrayList<>(), id);
+    }
+
+    public DiscordChatMessage(DiscordChatMessageAuthor author,
+                              String message,
+                              Long timestamp,
+                              DiscordChannel channel,
+                              List<DiscordChatCustomEmoticon> emoticons,
+                              DiscordEmbed embed,
+                              List<String> attachmentUrls,
+                              List<DiscordReaction> reactions,
+                              String id) {
         super(author, message, timestamp, channel, emoticons);
         this.id = id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
+        this.reactions = reactions;
+        this.embed = embed;
+        this.attachmentUrls = attachmentUrls;
     }
 
     /**
@@ -35,12 +58,32 @@ public class DiscordChatMessage extends ChatMessage<DiscordChatMessageAuthor, Di
         return id;
     }
 
+    /**
+     * @return optional container with the embed displayed in this chat message or an empty optional if this is a message without an embed
+     */
+    public Optional<DiscordEmbed> getEmbed() {
+        return Optional.ofNullable(embed);
+    }
+
+    /**
+     * @return a list containing the urls of the files attached to this message or an empty list if the message doesn't has an attachment
+     */
+    public List<String> getAttachments() {
+        return attachmentUrls;
+    }
+
+    /**
+     * @return a list containing all reactions of this chat message (for new messages this will be empty as users need some time to react on the message)
+     */
+    public List<DiscordReaction> getReactions() {
+        return reactions;
+    }
 
     @Override
     public String toHTMLString() {
         StringBuilder htmlMessage = new StringBuilder(getMessage());
         for (int i = getEmoticons().size() - 1; i >= 0; i--) {
-            DiscordChatCustomEmoticon emoticon = (DiscordChatCustomEmoticon) getEmoticons().get(i);
+            DiscordChatCustomEmoticon emoticon = getEmoticons().get(i);
             String htmlImage = emoticon.toHTMLString();
             htmlMessage.replace(emoticon.getIndex(), emoticon.getIndex() + emoticon.getRaw().length(), htmlImage);
         }
