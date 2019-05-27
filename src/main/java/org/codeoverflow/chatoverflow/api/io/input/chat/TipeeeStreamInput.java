d18375207298be74c07199a1cf5771b705391ac1
David <daullmer@gmail.com>
2019-05-27 18:47:19 +0200

    removed unnecessary classes

--- src/main/java/org/codeoverflow/chatoverflow/api/io/input/chat/TipeeeStreamInput.java
+++ src/main/java/org/codeoverflow/chatoverflow/api/io/input/chat/TipeeeStreamInput.java
@@ -3,11 +3,11 @@ package org.codeoverflow.chatoverflow.api.io.input.chat;
 import org.codeoverflow.chatoverflow.api.io.dto.event.tipeeestream.TipeeeStreamDonation;
 import org.codeoverflow.chatoverflow.api.io.dto.event.tipeeestream.TipeeeStreamFollow;
 import org.codeoverflow.chatoverflow.api.io.dto.event.tipeeestream.TipeeeStreamSubscription;
-import org.codeoverflow.chatoverflow.api.io.input.event.DonationEventInput;
+import org.codeoverflow.chatoverflow.api.io.input.Input;
 
 import java.util.function.Consumer;
 
-public interface TipeeeStreamInput extends DonationEventInput<TipeeeStreamDonation> {
+public interface TipeeeStreamInput extends Input {
     void registerSubscriptionHandler(Consumer<TipeeeStreamSubscription> handler);
     void registerDonationHandler(Consumer<TipeeeStreamDonation> handler);
     void registerFollowHandler(Consumer<TipeeeStreamFollow> handler);
