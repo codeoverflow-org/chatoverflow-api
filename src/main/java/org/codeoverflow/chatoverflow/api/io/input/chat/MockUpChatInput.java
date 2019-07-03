8c7bfe2feabfd64b3e4c7755d832a8cb5d4567ac
Jonas <joblo.2213@gmail.com>
2019-07-03 12:10:13 +0200

    API rework

--- src/main/java/org/codeoverflow/chatoverflow/api/io/input/chat/MockUpChatInput.java
+++ src/main/java/org/codeoverflow/chatoverflow/api/io/input/chat/MockUpChatInput.java
@@ -4,11 +4,12 @@ import org.codeoverflow.chatoverflow.api.io.dto.chat.TextChannel;
 import org.codeoverflow.chatoverflow.api.io.dto.chat.ChatEmoticon;
 import org.codeoverflow.chatoverflow.api.io.dto.chat.ChatMessage;
 import org.codeoverflow.chatoverflow.api.io.dto.chat.ChatMessageAuthor;
+import org.codeoverflow.chatoverflow.api.io.event.chat.mockup.MockupEvent;
 
 /**
  * A MockUp chat input for testing purposes, allows simulating public and private messages
  */
 public interface MockUpChatInput extends
-        ChatInput<ChatMessage<ChatMessageAuthor, TextChannel, ChatEmoticon>>,
-        PrivateChatInput<ChatMessage<ChatMessageAuthor, TextChannel, ChatEmoticon>> {
+        ChatInput<ChatMessage<ChatMessageAuthor, TextChannel, ChatEmoticon>, MockupEvent>,
+        PrivateChatInput<ChatMessage<ChatMessageAuthor, TextChannel, ChatEmoticon>, MockupEvent> {
 }
