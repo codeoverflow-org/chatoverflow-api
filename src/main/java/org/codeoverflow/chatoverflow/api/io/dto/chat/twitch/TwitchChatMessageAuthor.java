86530693eb708eabc69a506e4e9cc1f78e7a3980
MelanX <meissi97@me.com>
2019-07-11 23:20:05 +0200

    Added VIP role

--- src/main/java/org/codeoverflow/chatoverflow/api/io/dto/chat/twitch/TwitchChatMessageAuthor.java
+++ src/main/java/org/codeoverflow/chatoverflow/api/io/dto/chat/twitch/TwitchChatMessageAuthor.java
@@ -11,6 +11,7 @@ public class TwitchChatMessageAuthor extends ChatMessageAuthor {
     private final boolean broadcaster;
     private final boolean moderator;
     private final boolean subscriber;
+    private final boolean vip;
 
     public TwitchChatMessageAuthor(String displayName) {
         super(displayName);
@@ -18,24 +19,26 @@ public class TwitchChatMessageAuthor extends ChatMessageAuthor {
         this.broadcaster = false;
         this.moderator  = false;
         this.subscriber = false;
+        this.vip = false;
     }
 
     public TwitchChatMessageAuthor(String displayName, String color) {
-        this(displayName, color, false, false, false, false);
+        this(displayName, color, false, false, false, false, false);
     }
 
-    public TwitchChatMessageAuthor(String displayName, String color, boolean broadcaster, boolean moderator, boolean subscriber, boolean turbo) {
+    public TwitchChatMessageAuthor(String displayName, String color, boolean broadcaster, boolean moderator, boolean subscriber, boolean turbo, boolean vip) {
         super(displayName, displayName, color);
         this.turbo = turbo;
         this.broadcaster = broadcaster;
         this.moderator  = moderator;
         this.subscriber = subscriber;
+        this.vip = vip;
     }
 
     /**
      * @return true if the user has twitch turbo, false otherwise
      */
-    public boolean isTrubo() {
+    public boolean isTurbo() {
         return turbo;
     }
 
@@ -58,5 +61,12 @@ public class TwitchChatMessageAuthor extends ChatMessageAuthor {
      */
     public boolean isSubscriber() {
         return subscriber;
+    }
+
+    /**
+     * @return true if the user is a vip of the channel, false otherwise
+     */
+    public boolean isVIP() {
+        return vip;
     }
 }
