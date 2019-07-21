9c712ca4853782400bb7c152f8400b255124a36c
Felix <felixletsplay@outlook.de>
2019-07-21 13:01:10 +0200

    Add JavaDoc for RCON connector

--- src/main/java/org/codeoverflow/chatoverflow/api/io/input/RconInput.java
+++ src/main/java/org/codeoverflow/chatoverflow/api/io/input/RconInput.java
@@ -1,5 +1,14 @@
 package org.codeoverflow.chatoverflow.api.io.input;
 
 public interface RconInput extends Input {
+
+    /**
+     * Get the output of a rcon command
+     * Command will be directly executed on the rcon server and the returned response will be returned
+     * If null is returned this means that there is some problem with the connection (typically the server is shut down or there is some other reason for a lost connection)
+     * Notice: There is no way of knowing that a command exists, which does not return a response. Every command without response will return an empty string.
+     * @param command the command which should be executed on the remote server
+     * @return response of the server or null
+     */
     String getCommandOutput(String command);
 }
--- src/main/java/org/codeoverflow/chatoverflow/api/io/output/RconOutput.java
+++ src/main/java/org/codeoverflow/chatoverflow/api/io/output/RconOutput.java
@@ -2,5 +2,12 @@ package org.codeoverflow.chatoverflow.api.io.output;
 
 public interface RconOutput extends Output {
 
+
+    /**
+     * Execute a command on a remote (rcon) server.
+     * Notice: There is no way of knowing if the commands exists.
+     * @param command the command which should be executed
+     * @return if the command was successfully sent to the server. (Returns false for e.g. a not anymore existent server)
+     */
     boolean sendCommand(String command);
 }
