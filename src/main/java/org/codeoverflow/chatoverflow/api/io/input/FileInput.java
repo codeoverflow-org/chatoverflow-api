93cccba3adfa0cea755990f10706f7dbc258243f
Jonas <joblo.2213@gmail.com>
2019-10-31 18:13:28 +0100

    Add javadoc for FileInput and FileOutput

--- src/main/java/org/codeoverflow/chatoverflow/api/io/input/FileInput.java
+++ src/main/java/org/codeoverflow/chatoverflow/api/io/input/FileInput.java
@@ -5,13 +5,32 @@ import org.codeoverflow.chatoverflow.api.IsRequirement;
 import java.awt.image.BufferedImage;
 import java.util.Optional;
 
+/**
+ * Input that allows you to perform file operations that would otherwise be prohibited by the sandbox security manager.<br>
+ * It features easy ways to read different types of files
+ */
 @IsRequirement(requires = "file system access")
 public interface FileInput extends Input {
 
+    /**
+     * Load a text file and return the content as string
+     * @param pathInResources the relative path to the file in the data folder
+     * @return the content of the file or an empty optional if the operation failed
+     */
     Optional<String> getFile(String pathInResources);
 
+    /**
+     * Load an image from a file and return it as BufferedImage
+     * @param pathInResources the relative path to the file in the data folder
+     * @return the image or an empty optional if the operation failed
+     */
     Optional<BufferedImage> getImage(String pathInResources);
 
+    /**
+     * Load any file into a byte array
+     * @param pathInResources the relative path to the file in the data folder
+     * @return a byte array that contains all data from the file or an empty optional if the operation failed
+     */
     Optional<byte[]> getBinaryFile(String pathInResources);
 
 }
--- src/main/java/org/codeoverflow/chatoverflow/api/io/output/FileOutput.java
+++ src/main/java/org/codeoverflow/chatoverflow/api/io/output/FileOutput.java
@@ -2,21 +2,61 @@ package org.codeoverflow.chatoverflow.api.io.output;
 
 import org.codeoverflow.chatoverflow.api.IsRequirement;
 
+import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 
+/**
+ * Output that allows you to perform file operations that would otherwise be prohibited by the sandbox security manager.<br>
+ * It features easy ways to save all kinds of files, creat or delete directories or check if a file exists.
+ */
 @IsRequirement(requires = "file system access")
 public interface FileOutput extends Output {
 
+    /**
+     * Save a BufferedImage to a file
+     * @param image the image that should be saved
+     * @param format a string containing the informal name of the format (e.g. PNG / JPG / GIF / BMP)<br>
+     *               use {@link ImageIO#getWriterFormatNames()} to list all valid ones
+     * @param pathInResources the relative path to the file in the data folder
+     * @return if the operation was successful
+     */
     boolean saveImage(BufferedImage image, String format, String pathInResources);
 
+    /**
+     * Save any byte array to a file.
+     * @param bytes raw content that should be saved to the file
+     * @param pathInResources the relative path to the file in the data folder
+     * @return if the operation was successful
+     */
     boolean saveBinaryFile(byte[] bytes, String pathInResources);
 
+    /**
+     * Save a String to a text file
+     * @param content the text that should be saved
+     * @param pathInResources the relative path to the file in the data folder
+     * @return if the operation was successful
+     */
     boolean saveFile(String content, String pathInResources);
 
+    /**
+     * Create a new folder inside the data folder
+     * @param folderName the relative path to the folder in the data folder
+     * @return if the operation was successful
+     */
     boolean createDirectory(String folderName);
 
+    /**
+     * Check if a file or folder does exist
+     * @param pathInResources the relative path to the file in the data folder
+     * @return if the file does exist
+     */
     boolean exists(String pathInResources);
 
+    /**
+     * Delete a file or a folder
+     * @param pathInResources the relative path to the file in the data folder
+     * @return if the operation was successful
+     */
     boolean delete(String pathInResources);
 
 }
