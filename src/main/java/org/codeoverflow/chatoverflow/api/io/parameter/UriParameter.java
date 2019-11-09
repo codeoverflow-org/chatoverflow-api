3912a1fae57bf6206313cb32352ee459cdfb3097
derNiklaas <derNiklaas@users.noreply.github.com>
2019-11-09 12:47:15 +0100

    Added URI parameter

---
+++ src/main/java/org/codeoverflow/chatoverflow/api/io/parameter/UriParameter.java
@@ -0,0 +1,9 @@
+package org.codeoverflow.chatoverflow.api.io.parameter;
+
+import org.codeoverflow.chatoverflow.api.IsRequirement;
+
+import java.net.URI;
+
+@IsRequirement
+public interface UriParameter extends Parameter<URI> {
+}
