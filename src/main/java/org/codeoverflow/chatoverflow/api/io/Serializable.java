a512a2bf3d43126b6858ba905ec3fe77ba48893b
Daniel <daniel.hub@outlook.de>
2019-11-09 15:02:32 +0100

    Add comment to the deserialize method of Serializable about exception throwing

--- src/main/java/org/codeoverflow/chatoverflow/api/io/Serializable.java
+++ src/main/java/org/codeoverflow/chatoverflow/api/io/Serializable.java
@@ -8,14 +8,15 @@ public interface Serializable {
     /**
      * Serializes this object into a string to save it to a config
      *
-     * @return serialized
+     * @return serialized data
      */
     String serialize();
 
     /**
      * Deserialize a string to apply provided config settings to this object
+     * If the string can't be deserialized this method should thrown an exception
      *
-     * @param value should be serialized
+     * @param value the string that should be deserialized
      */
     void deserialize(String value);
 }
