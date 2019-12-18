dd892aaa0081cbde7a0f28edbddf2cca832627ec
Adrian Nöthlich <git@promasu.tech>
2019-12-18 02:08:02 +0100

    Add documentation
    Signed-off-by: Adrian Nöthlich <git@promasu.tech>

--- src/main/java/org/codeoverflow/chatoverflow/api/io/output/twitter/TwitterTweetOutput.java
+++ src/main/java/org/codeoverflow/chatoverflow/api/io/output/twitter/TwitterTweetOutput.java
@@ -6,10 +6,27 @@ import org.codeoverflow.chatoverflow.api.io.output.Output;
 
 import java.awt.image.BufferedImage;
 
+/**
+ * The TwitterTweetOutput allows you to send tweets with or without image
+ */
+
 @IsRequirement(requires = "twitter chat bot")
 public interface TwitterTweetOutput extends Output {
 
+
+    /**
+     * Sends a plaintext tweet
+     *
+     * @param status Text that should be tweeted
+     */
     boolean sendTweet(String status);
+
+    /**
+     * Sends a tweet with an image
+     *
+     * @param status Text that should be tweeted
+     * @param image  BufferedImage that should be tweeted
+     */
     boolean sendImageTweet(String status, BufferedImage image);
 
 }
